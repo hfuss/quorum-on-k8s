cf5b26690fb832064bf53b84aabd716f6dde7633
W Hayden Fuss <wifu1234@gmail.com>
2021-02-09 01:58:58 -0500

    Working Racecourse Chart

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .PHONY: clean setup deploy test k8s build debug
 
 clean:
+	helm delete -n kaleido racecourse || true
 	helm delete -n kaleido quorum || true
 	kubectl delete --ignore-not-found -n kaleido pvc,configmap,job,pod -l 'app.kubernetes.io/name=quorum'
 
@@ -12,9 +13,11 @@ deploy:
 
 test:
 	helm test quorum -n kaleido --logs
+	helm test racecourse -n kaleido --logs
 
 debug:
 	helm template --debug /charts/quorum
+	helm template --debug /charts/racecourse
 
 k8s:
 	./hack/start-minikube.sh
--- charts/quorum/scripts/test-public-contract.sh
+++ charts/quorum/scripts/test-public-contract.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# wait for quorum to start w/ fibonocci backoff
+i=1
+j=1
+until [[ -e "/qdata/ethereum/geth.ipc" ]]; do
+  echo "geth has not created socket, waiting $j sec before re-attempting to run test"
+  sleep $j
+  k=$j
+  j=$(($k + $i))
+  i=$k
+done
+
 echo "Attempting to mine public contract"
 # we can't tell from the return code if the script succeeds, but it does print "true"
 geth attach /qdata/ethereum/geth.ipc --exec "loadScript('/scripts/public-contract.js')" | grep true
